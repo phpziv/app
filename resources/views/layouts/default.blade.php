072ed6a43e555e655f5ce2df4519f95e6bcbec2e
Your Name <you@example.com>
2019-04-01 16:04:40 +0800

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
 
@@ -15,5 +14,7 @@
         @include('layouts._footer')
       </div>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
