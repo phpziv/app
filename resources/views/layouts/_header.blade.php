73862205bf3421953b69459c5a7ef807adb62c9a
Your Name <you@example.com>
2019-04-01 14:36:25 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
             <div class="container ">
               <a class="navbar-brand" href="/">App</a>
               <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help">关于我们</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">关于我们</a></li>
                 <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
               </ul>
             </div>
